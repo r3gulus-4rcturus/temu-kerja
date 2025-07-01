a94df0010f983fd6f36e15927d877e028be59302
Muhammad Lanang <muh.lanangzalkifla@gmail.com>
2025-07-01 13:55:45 +0700

    Configure and import fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,16 @@
-import { Inter } from "next/font/google"
+import {
+  Barlow,
+  Hind,
+  Inter,
+  Plus_Jakarta_Sans,
+  Poppins,
+  Public_Sans,
+  Roboto,
+  Source_Code_Pro,
+  Ubuntu,
+  Urbanist
+} from 'next/font/google';
 import "./globals.css"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Temu Kerja - Cari Kerja Jadi Lebih Mudah dan Cepat",
@@ -9,10 +18,22 @@ export const metadata = {
     "Platform yang menghubungkan individu yang mencari penghasilan dengan orang-orang yang butuh bantuan. Mulai dari jasa bersih-bersih hingga proyek profesional.",
 }
 
+// Import configs:
+const barlow = Barlow({ weight: ['500'], subsets: ['latin'], variable: '--font-barlow' });
+const hind = Hind({ weight: ['400'], subsets: ['latin'], variable: '--font-hind' });
+const inter = Inter({ weight: ['400', '500', '600'], subsets: ['latin'], variable: '--font-inter' });
+const plusJakarta = Plus_Jakarta_Sans({ weight: ['700', '800'], subsets: ['latin'], variable: '--font-plus-jakarta' });
+const poppins = Poppins({ weight: ['500'], subsets: ['latin'], variable: '--font-poppins' });
+const publicSans = Public_Sans({ weight: ['400'], subsets: ['latin'], variable: '--font-public-sans' });
+const roboto = Roboto({ weight: ['400', '500', '600'], subsets: ['latin'], variable: '--font-roboto' });
+const sourceCodePro = Source_Code_Pro({ weight: ['400'], subsets: ['latin'], variable: '--font-source-code-pro' });
+const ubuntu = Ubuntu({ weight: ['700'], subsets: ['latin'], variable: '--font-ubuntu' });
+const urbanist = Urbanist({ weight: ['700', '600'], subsets: ['latin'], variable: '--font-urbanist' });
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
-      <body className={inter.className}>{children}</body>
+    <html lang="id" className={`${barlow.variable} ${hind.variable} ${inter.variable} ${plusJakarta.variable} ${poppins.variable} ${publicSans.variable} ${roboto.variable} ${sourceCodePro.variable} ${ubuntu.variable} ${urbanist.variable}`}>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
